b89ca3ae424c5632c5d181b491d36a9d6cb8cada
jeremias <jeremias.cuta@virtuespin.com>
2019-07-11 16:17:36 -0300

    agrego tabla de estados y de facturas

--- sistema/admin.py
+++ sistema/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
-from .models import TipoGasto, Gasto
+from .models import TipoGasto, Gasto, Estado, Factura
+
+
 # Register your models here.
 class GastoAdmin(admin.ModelAdmin):
-    list_display =  ('fecha', 'tipo', 'concepto', 'monto')
-    list_filter = ['fecha', 'tipo']
+    list_display =  ('fecha', 'tipo', 'estado', 'concepto', 'monto')
+    list_filter = ['fecha', 'tipo', 'estado']
     search_fields = ['concepto']
 
+
+class FacturaAdmin(admin.ModelAdmin):
+    list_display =  ('fecha', 'numero', 'estado', 'monto')
+    list_filter = ['fecha', 'numero', 'estado']
+    search_fields = ['numero']
+
+
 admin.site.register(TipoGasto)
+admin.site.register(Estado)
+admin.site.register(Factura,FacturaAdmin)
 admin.site.register(Gasto,GastoAdmin)
--- sistema/models.py
+++ sistema/models.py
@@ -1,18 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class Estado(models.Model):
+    estado = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.estado
+
+
 class TipoGasto(models.Model):
     tipo = models.CharField(max_length=200)
 
     def __str__(self):
         return self.tipo
 
+
 class Gasto(models.Model):
     fecha = models.DateField('fecha')
     tipo = models.ForeignKey(TipoGasto, on_delete=models.SET_NULL, null=True, blank=True)
+    estado = models.ForeignKey(Estado, on_delete=models.SET_NULL, null=True, blank=True)
     concepto = models.CharField(max_length=200)
     monto = models.IntegerField(default=0)
+    comprobante = models.ImageField(upload_to='comprobante', null=True, blank=True)
 
     def __str__(self):
         return 'fecha: ' + self.fecha + ' tipo: ' + self.tipo 
 
+
+class Factura(models.Model):
+    fecha = models.DateField('fecha')
+    numero = models.CharField(max_length=200)
+    estado = models.ForeignKey(Estado, on_delete=models.SET_NULL, null=True, blank=True)
+    monto = models.IntegerField(default=0)
+    factura = models.ImageField(upload_to='facturas', null=True, blank=True)
+
+    def __str__(self):
+        return 'fecha: ' + self.fecha + ' numero: ' + self.numero 
